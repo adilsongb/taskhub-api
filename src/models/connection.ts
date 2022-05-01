5d39fd1bf4aa60b47cb733cc16d500d46b3d29de
Adilson Gabriel <adilsongb.rabelo@gmail.com>
2022-05-01 11:27:34 -0300

    feat: testa docker compose

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+/* import mongoose from 'mongoose';
 
 const MONGO_DB_URL = 'mongodb://localhost:27017/taskhub';
 
@@ -6,4 +6,4 @@ const connectToDatabase = (
   mongoDatabaseURI = process.env.MONGO_URI || MONGO_DB_URL,
 ) => mongoose.connect(mongoDatabaseURI);
 
-export default connectToDatabase;
+export default connectToDatabase; */
