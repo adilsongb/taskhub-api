9e7b117a8e0097d60badcac03047c804f4e395e4
Adilson Gabriel <adilsongb.rabelo@gmail.com>
2022-05-02 23:02:10 -0300

    feat: cria conexao com o banco mongo

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
-import connectToDatabase from './models/connection';
+import connectToDatabase from './database/connection';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.status(200).json({ message: 'É sobre isso' });
+app.get('/', (_req: Request, res: Response) => {
+  res.status(200).json({ message: 'sucess' });
 });
 
 connectToDatabase();
